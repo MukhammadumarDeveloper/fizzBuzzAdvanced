a955d12bbfacbeec91fcc9fd806342b2dae5ff77
ilhomjon55 <ilhomshamidinov55@gmail.com>
2020-11-05 07:45:35 +0500

    Add js side

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 var elFormMain = document.querySelector('.form-main');
 var elInputMain = elFormMain.querySelector('.input-main');
+var elResultFizzLength = elFormMain.querySelector('.result-fizz-length');
+var elResultFizz = elFormMain.querySelector('.result-fizz');
+var elResultBuzzLength = elFormMain.querySelector('.result-buzz-length');
+var elResultBuzz = elFormMain.querySelector('.result-buzz');
+var elResultFizzBuzzLength = elFormMain.querySelector('.result-fizzBuzz-length');
+var elResultFizzBuzz = elFormMain.querySelector('.result-fizzBuzz');
 
 
 
-elFormMain.addEventListener('submit', function (evt) {
+var calculateFizzBuzz = function (evt) {
   evt.preventDefault();
 
 
@@ -21,8 +27,34 @@ elFormMain.addEventListener('submit', function (evt) {
     } else if ((i % 3 === 0) && (i % 5 === 0)) {
       fizzBuzzArray.push(i);
     }
+  };
+
+
+  elResultFizz.textContent = '0, ';
+  elResultBuzz.textContent = '0, ';
+  elResultFizzBuzz.textContent = '0, ';
+
+  for (element of fizzArray) {
+    elResultFizz.textContent += `${element}, `
   }
+  for (element of buzzArray) {
+    elResultBuzz.textContent += `${element}, `
+  }
+  for (element of fizzBuzzArray) {
+    elResultFizzBuzz.textContent += `${element}, `
+  }
+
+
+  elResultFizzLength.textContent = `(soni) ${fizzArray.length} ta`;
+
+  elResultBuzzLength.textContent = `(soni) ${buzzArray.length} ta`;
+
+  elResultFizzBuzzLength.textContent = `(soni) ${fizzBuzzArray.length} ta`;
+
+
   console.log(fizzArray, fizzArray.length);
   console.log(buzzArray, buzzArray.length);
   console.log(fizzBuzzArray, fizzBuzzArray.length);
-})
+};
+
+elFormMain.addEventListener('submit', calculateFizzBuzz);
